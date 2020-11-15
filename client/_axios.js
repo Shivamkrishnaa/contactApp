6c63ed74641a8516f5fe6e44f9ab8ddb53db1a48
Glitch (fog-near-piper) <none>
2020-11-15 21:25:41 +0000

    👦💏 Checkpoint

--- client/_axios.js
+++ client/_axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/';
+const API_URL = 'https://polarized-false-plier.glitch.me/'||'http://localhost:4000/';
 const instance = axios.create({ baseURL: API_URL, headers: {
     'Content-Type': 'application/json'
   },        withCredentials: true});
