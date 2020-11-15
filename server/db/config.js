8261fd0644d4af9d66f4edb192f7925d79f95b95
Shivam krishna <shivam.krishna@kratikal.com>
2020-11-16 01:13:05 +0530

    fix

--- server/db/config.js
+++ server/db/config.js
@@ -5,5 +5,5 @@ module.exports = {
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
 	host: process.env.DB_HOST,
-	dialect: process.env.DB_CONNECTION
+	dialect: process.env.DB_CONNECTION || 'sqlite'
 }
