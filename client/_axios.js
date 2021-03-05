96e032d4603a645dacad15716115d80ace0533f8
shivamkrishna <74373566+shivamkrishna@users.noreply.github.com>
2021-03-05 22:19:07 +0530

    client rm

--- client/_axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:4000/';
-const instance = axios.create({ baseURL: API_URL, headers: {
-    'Content-Type': 'application/json'
-  },        withCredentials: true});
-
-export default instance;
