dcf3c2983a83820688fed25402c65446c4c26400
Glitch (fog-near-piper) <none>
2021-03-05 21:18:14 +0000

    Import of Shivamkrishnaa/contactApp

--- _axios.js
+++ _axios.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-const API_URL = 'https://relieved-lilac-jaborosa.glitch.me/';
-=======
 const API_URL = process.env.NEXT_PUBLIC_API||'http://localhost:4000/';
->>>>>>> 4efd8ea6ebb96394bc5d306d8eb2828106c16d72
 const instance = axios.create({ baseURL: API_URL, headers: {
     'Content-Type': 'application/json'
   },        withCredentials: true});
